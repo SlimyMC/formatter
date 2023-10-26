98382f3073444c36f58101767bbc4cff3230ac05
Hasan Demirtaş <utsukushihito@outlook.com>
2023-10-26 16:25:07 +0300

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    alias(libs.plugins.spotless)
+}
+
+repositories {
+    mavenCentral()
+}
+
+spotless {
+    lineEndings = com.diffplug.spotless.LineEnding.UNIX
+
+    format("encoding") {
+        target("*.*")
+        encoding("UTF-8")
+        endWithNewline()
+        trimTrailingWhitespace()
+    }
+
+    yaml {
+        target(
+            ".github/**/*.yml",
+            ".github/**/*.yaml",
+        )
+        endWithNewline()
+        trimTrailingWhitespace()
+        jackson()
+            .yamlFeature("LITERAL_BLOCK_STYLE", true)
+            .yamlFeature("MINIMIZE_QUOTES", true)
+            .yamlFeature("SPLIT_LINES", false)
+    }
+
+    kotlinGradle {
+        target("**/*.gradle.kts")
+        endWithNewline()
+        trimTrailingWhitespace()
+        diktat()
+    }
+
+    java {
+        target("**/src/**/java/**/*.java")
+        importOrder()
+        removeUnusedImports()
+        indentWithSpaces(2)
+        endWithNewline()
+        trimTrailingWhitespace()
+        eclipse().configFile("eclipse-prefs.xml")
+    }
+}
