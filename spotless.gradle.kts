a61c78ebc9fc933ffd31fb71104fb97344db90c2
Hasan Demirtaş <utsukushihito@outlook.com>
2023-10-28 02:23:30 +0300

    Update spotless.gradle.kts

--- spotless.gradle.kts
+++ spotless.gradle.kts
@@ -1,59 +1,59 @@
-apply<com.diffplug.gradle.spotless.SpotlessPlugin>()
+apply {
+    plugin("com.diffplug.spotless")
+}
 
-println("----------")
-//
-//spotless {
-//    lineEndings = com.diffplug.spotless.LineEnding.UNIX
-//
-//    format("encoding") {
-//        target("*.*")
-//        encoding("UTF-8")
-//        endWithNewline()
-//        trimTrailingWhitespace()
-//    }
-//
-//    yaml {
-//        target(
-//            ".github/**/*.yml",
-//            ".github/**/*.yaml",
-//        )
-//        endWithNewline()
-//        trimTrailingWhitespace()
-//        jackson()
-//            .yamlFeature("LITERAL_BLOCK_STYLE", true)
-//            .yamlFeature("MINIMIZE_QUOTES", true)
-//            .yamlFeature("SPLIT_LINES", false)
-//    }
-//
-//    kotlinGradle {
-//        target("**/*.gradle.kts")
-//        endWithNewline()
-//        trimTrailingWhitespace()
-//        //diktat()
-//    }
-//
-//    java {
-//        target("**/src/**/java/**/*.java")
-//        importOrder()
-//        removeUnusedImports()
-//        indentWithSpaces(2)
-//        endWithNewline()
-//        trimTrailingWhitespace()
-//
-//        val url = "https://raw.githubusercontent.com/SlimyMC/formatter/fix/url/eclipse-prefs.xml"
-//        val localFilePath = project.layout.projectDirectory
-//            .dir(".gradle/caches/formatter")
-//            .file("eclipse-prefs.xml")
-//            .asFile
-//        val urlConnection = URL(url).openConnection()
-//        urlConnection.connect()
-//        urlConnection.getInputStream().use { inputStream ->
-//            localFilePath.parentFile?.mkdirs()
-//            localFilePath.outputStream().use { outputStream ->
-//                inputStream.copyTo(outputStream)
-//            }
-//        }
-//
-//        eclipse().configFile(localFilePath)
-//    }
-//}
+spotless {
+    lineEndings = com.diffplug.spotless.LineEnding.UNIX
+
+    format("encoding") {
+        target("*.*")
+        encoding("UTF-8")
+        endWithNewline()
+        trimTrailingWhitespace()
+    }
+
+    yaml {
+        target(
+            ".github/**/*.yml",
+            ".github/**/*.yaml",
+        )
+        endWithNewline()
+        trimTrailingWhitespace()
+        jackson()
+            .yamlFeature("LITERAL_BLOCK_STYLE", true)
+            .yamlFeature("MINIMIZE_QUOTES", true)
+            .yamlFeature("SPLIT_LINES", false)
+    }
+
+    kotlinGradle {
+        target("**/*.gradle.kts")
+        endWithNewline()
+        trimTrailingWhitespace()
+        //diktat()
+    }
+
+    java {
+        target("**/src/**/java/**/*.java")
+        importOrder()
+        removeUnusedImports()
+        indentWithSpaces(2)
+        endWithNewline()
+        trimTrailingWhitespace()
+
+        val url = "https://raw.githubusercontent.com/SlimyMC/formatter/fix/url/eclipse-prefs.xml"
+        val localFilePath = project.layout.projectDirectory
+            .dir(".gradle/caches/formatter")
+            .file("eclipse-prefs.xml")
+            .asFile
+        val urlConnection = URL(url).openConnection()
+        urlConnection.connect()
+        urlConnection.getInputStream().use { inputStream ->
+            localFilePath.parentFile?.mkdirs()
+            localFilePath.outputStream().use { outputStream ->
+                inputStream.copyTo(outputStream)
+            }
+        }
+
+        eclipse().configFile(localFilePath)
+    }
+}
